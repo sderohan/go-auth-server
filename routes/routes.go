872aeccb6c401a9153f38089b4a4b0ee5ce9dac4
sderohan <rohanshinde7212@gmail.com>
2022-06-02 16:41:51 +0530

    feat: project boilerplate setup and add api routes

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,17 @@
 package routes
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var notImplemented = func(w http.ResponseWriter, r *http.Request) {}
+
+func InitRoutes() *mux.Router {
+	rts := mux.NewRouter()
+	v1 := rts.PathPrefix("/v1").Subrouter()
+	v1.HandleFunc("/getToken", notImplemented)
+	v1.HandleFunc("/validateToken", notImplemented)
+	return rts
+}
