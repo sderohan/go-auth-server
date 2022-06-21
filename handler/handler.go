669300b47aaff8c3e58fe011e9f42c971b055eae
sderohan <rohanshinde7212@gmail.com>
2022-06-21 19:44:50 +0530

    remove unnecessary handler code

--- handler/handler.go
+++ handler/handler.go
@@ -1,12 +1,7 @@
 package handler
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
-
-	"github.com/sderohan/go-auth-server/pkg/models"
-	"github.com/sderohan/go-auth-server/pkg/validator"
 )
 
 type IAuthHandler interface {
@@ -19,28 +14,12 @@ type AuthHandler struct {
 }
 
 func (auth AuthHandler) SignIn(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "User registration")
 }
 
 func (auth AuthHandler) SingOut(w http.ResponseWriter, r *http.Request) {
-	var request models.AuthRequest
-	err := json.NewDecoder(r.Body).Decode(&request)
-	defer r.Body.Close()
-	if err != nil {
-		fmt.Println("error occured while marshaling the request")
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	validationErrors := validator.Validate(request)
-	if validationErrors != nil {
-		json.NewEncoder(w).Encode(validationErrors)
-		return
-	}
-	json.NewEncoder(w).Encode(request)
 }
 
 func (auth AuthHandler) Validate(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "token validation")
 }
 
 func NewAuthHandler() IAuthHandler {
